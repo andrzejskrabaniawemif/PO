b24a8da64dab051410e83af7855c95a3ee557b6a
andrzej.skrabania@gmail.com <andrzej.skrabania@gmail.com>
2020-04-29 00:59:16 +0200

    zaimplementowane metody bez wzorow do klasy analysis

--- Classes/Analysis.cs
+++ Classes/Analysis.cs
@@ -7,5 +7,24 @@ namespace DietCalc.Classes
 {
     public class Analysis
     {
+        public int target { get; set; }
+        public int targetWeight { get; set; }
+        public double caloriesChange { get; set; }
+        public double weightChange { get; set; }
+
+        public double CalcProtein(double weight, int activity, int target)
+        {
+            return weight * activity * target;
+        }
+
+        public double CalcFat(double weight, int activity, int target)
+        {
+            return weight * activity * target;
+        }
+
+        public double CalcCarbo(double weight, int activity, int target)
+        {
+            return weight * activity * target;
+        }
     }
 }
